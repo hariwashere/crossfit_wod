b5f76eb404b93703ca462e9e1fb903e404d1fecc
Rajeshwari Tamaskar <tamaskar@yelp.com>
2016-07-21 15:23:01 -0700

    Correct template rendering code

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
+import os
 import wsgiref.handlers
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-class MyHandler(webapp.RequestHandler):
+
+class WorkoutSession(db.Model):
+    workoutType = db.StringProperty(required=True)
+    workoutTime = db.DateTimeProperty(required=True)
+
+
+class MyWorkoutSession(webapp.RequestHandler):
 	def get(self):
-		content = template.render('main.html', {})
-		self.response.out.write('hello wodsup')
+#       list_of_equipment = self.request.get('list_of_equipment')
+		self.response.out.write(unicode(template.render('wod.html', {})))
+#		self.response.out.write('hello wodsup')
+
 
 def main():
 	app = webapp.WSGIApplication([
-		(r'.*', MyHandler)], debug=True)
+		(r'.*', MyWorkoutSession)], debug=True)
 	wsgiref.handlers.CGIHandler().run(app)
 
 if __name__ == "__main__":
