f9d03be1333e9d4aaa6cf9a4be8e1ad47d6fc75c
Shrihari <hari@hari.com>
2016-07-21 15:18:53 -0700

    Scrape unwated texts away

--- scraper.py
+++ scraper.py
@@ -1,15 +1,22 @@
 from lxml import html
 import requests
 
+
+def get_exercise_text(exercise_dom):
+    exercise_string = unicode(exercise_dom.text_content()).encode('utf-8')
+    # import ipdb; ipdb.set_trace()
+    return exercise_string[:exercise_string.find("Post")]
+
+
 BASE_URL = 'https://www.crossfit.com/workout'
 page = requests.get(BASE_URL)
 tree = html.fromstring(page.content)
 exercise_doms = tree.find_class('col-sm-6')
 
+# Weed out matches for the entire exercise section
+exercise_doms = filter(lambda exercise: len(exercise.classes) == 1, exercise_doms)
+
+exercise_text = map(get_exercise_text, exercise_doms)
+
 with open('exercises', 'w') as op_file:
-    for exercise in exercise_doms:
-        # Weed out matches for the entire exercise section
-        if len(exercise.classes) == 1:
-            op_file.write(
-                unicode(exercise.text_content()).encode('utf-8')
-            )
+    op_file.writelines(exercise_text)
