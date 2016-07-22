c035423240ce63e40b10ed36345fa385bc1c073b
Shrihari <hari@hari.com>
2016-07-22 12:28:28 -0700

    Write wods with equipments to a file

--- scraper.py
+++ scraper.py
@@ -26,13 +26,10 @@ def master():
     page = requests.get(BASE_URL)
     tree = html.fromstring(page.content)
     exercise_doms = tree.find_class('col-sm-6')
-    
+
     # Weed out matches for the entire exercise section
     exercise_doms = filter(lambda exercise: len(exercise.classes) == 1, exercise_doms)
     exercise_text = map(get_exercise_text, exercise_doms)
-
-    with open('exercises', 'w') as op_file:
-        op_file.writelines(exercise_text)
 
     wod_to_equipment = []
 
@@ -40,11 +37,15 @@ def master():
         if len(wod) < 10 or wod.startswith('Rest Day'):
             continue
         else:
-            equipment = convert_wod_to_equipment(wod)
+            equipment = get_equipments_for_wod(wod)
 
         wod_to_equipment.append({'wod': wod, 'equipment': equipment})
 
-    return wod_to_equipment
+    with open('exercises', 'w') as op_file:
+        op_file.writelines(
+            [repr(line) for line in wod_to_equipment]
+        )
+    return
 
 
 def convert_wod_to_equipment(wod):
@@ -61,16 +62,21 @@ def convert_wod_to_equipment(wod):
     return list(set(equipment))
 
 
-def get_movements_with_equipment(exercise_text):
-    matched_movements = []
+def get_equipments_for_wod(exercise_text):
+    equipments_needed = []
     for movement, movement_regex in M2E.MOVEMENT_TO_REGEX.iteritems():
         if re.search(movement_regex, exercise_text.lower()):
-            matched_movements.append(movement)
+            if (M2E.MOVEMENT_TO_EQUIPMENT.get(movement)):
+                equipments_needed.append(
+                    M2E.MOVEMENT_TO_EQUIPMENT.get(movement)
+                )
+    return equipments_needed
 
-    return matched_movements
 
 
 def get_exercise_text(exercise_dom):
     exercise_string = unicode(exercise_dom.text_content()).encode('utf-8')
     return exercise_string[:exercise_string.find("Post")]
 
+if __name__ == "__main__":
+    master()
